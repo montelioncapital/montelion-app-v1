6119f2777c829bc8302a21f86f8138fb920f5288
montelioncapital <montelion.capital@gmail.com>
2025-11-11 23:43:49 +0100

    Update middleware.js

--- app/middleware.js
+++ app/middleware.js
@@ -1,11 +1,6 @@
 // app/middleware.js
 import { NextResponse } from "next/server";
-
 export function middleware() {
   return NextResponse.next();
 }
-
-// Optionnel : limite le middleware au dashboard seulement
-export const config = {
-  matcher: ["/dashboard/:path*"],
-};
+export const config = { matcher: [] };
