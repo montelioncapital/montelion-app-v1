7b3e69fb065ea089f9a76dac8fefdbb75cbebe96
montelioncapital <montelion.capital@gmail.com>
2025-11-11 23:57:57 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,8 @@
+// app/layout.jsx
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Montelion Capital",
@@ -8,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr" className="dark">
-      <body className="min-h-screen antialiased bg-black text-white">
-        {children}
-      </body>
+      <body className={`${inter.className} bg-app min-h-dvh`}>{children}</body>
     </html>
   );
 }
