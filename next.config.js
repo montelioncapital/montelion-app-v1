83d286006629350a156c8965c530925d26802223
montelioncapital <montelion.capital@gmail.com>
2025-11-12 00:12:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // On tourne en mode serveur Node, pas d'export statique
-  output: 'standalone', // (optionnel mais top pour Docker)
+  output: "standalone",
   reactStrictMode: true,
 };
 
