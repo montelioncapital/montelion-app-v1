dca15c5f21cab5f0261c6e4566e9c4df419a1d18
montelioncapital <montelion.capital@gmail.com>
2025-11-12 16:07:11 +0100

    Update supabaseClient.ts

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,15 +1,13 @@
-// app/lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
+// app/lib/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,        // déjà dans ton .env.local
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,  // idem
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   {
     auth: {
       persistSession: true,
       autoRefreshToken: true,
-      detectSessionInUrl: true,
-      storageKey: 'montelion.auth'             // clé locale
-    }
+    },
   }
 );
